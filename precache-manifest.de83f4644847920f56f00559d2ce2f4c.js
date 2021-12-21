0b5f80bf6e4b31f2a9c3db652f25fc1c736501cf
vivlai <vivwylai@gmail.com>
2021-12-21 00:57:24 -0600

    Updates

---
+++ precache-manifest.de83f4644847920f56f00559d2ce2f4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79cf311551f7b475313882e8259e49df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ee7ac7b474b0847230a",
+    "url": "/static/css/2.d6d128f2.chunk.css"
+  },
+  {
+    "revision": "0536a777a42a14bc5070",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ee7ac7b474b0847230a",
+    "url": "/static/js/2.b726cc2b.chunk.js"
+  },
+  {
+    "revision": "0536a777a42a14bc5070",
+    "url": "/static/js/main.71644803.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
